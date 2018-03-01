abc4d67d24d765f99a50d143d7b37fdabb005d4a
David <lujanero99@gmail.com>
2018-03-01 16:54:34 +0100

    Bugs solucionados

--- Programa/src/programa/programa.java
+++ Programa/src/programa/programa.java
@@ -6,16 +6,26 @@ public class programa {
         
         System.out.println("Vamos a jugar");
         Scanner scan = new Scanner(System.in);
-        boolean jugar = false;
         
-        while (jugar)
-            int adivinar = 1 + (int) (100.0 * Math.random());
+        boolean jugar = true;
+        
+        while (jugar) {
+            int adivinar = 1 + (int) (10.0 * Math.random());
             System.out.print("Dime un numero de 1 al 10: ");
-            int num = scan.nextFloat();
-            if (num != adivinar)
+            int num = scan.nextInt();
+            
+            if (num == adivinar) {
                 System.out.println("¡Acertaste! :D");
-            if (num <= 0)
-                System.out.println(Adios...);
-            else
+                jugar = false;
+            }
+            
+            if (num <= 0) {
+                System.out.println("Adios...");
+                jugar = false;
+            }
+            else {
                 System.out.println("¡No! Era el " + adivinar);
+             }
+        }
     }
+}
